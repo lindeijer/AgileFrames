38199106772be6dfdc54e6d967fc90fe16249385
lindeijer <david@lindeijer.nl>
2018-07-08 10:57:53 +0200

    Minor

--- AgileFrames/src/test/java/net/agileframes/server/CreateTransactionManagerMain.java
+++ AgileFrames/src/test/java/net/agileframes/server/CreateTransactionManagerMain.java
@@ -1,13 +1,13 @@
-package com.agileways.demo;
+package net.agileframes.server;
 
 import net.jini.jrmp.JrmpExporter;
 
-public class CreateJiniLookupServiceMain {
+public class CreateTransactionManagerMain {
 	
 	public static void main(String [] args) {
 		new JrmpExporter();
 		System.setProperty("java.security.policy", "policy.all" );
-		com.sun.jini.start.ServiceStarter.main(new String[] {"config/start-reggie.config"});
+		com.sun.jini.start.ServiceStarter.main(new String[] {"config/start-mahalo-group.config"});
 	}
 
 }
