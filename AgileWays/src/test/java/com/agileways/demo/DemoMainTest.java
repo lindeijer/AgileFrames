575164fe38a68f75985a6c0f2a8b846f93388777
lindeijer <david@lindeijer.nl>
2018-07-15 13:01:32 +0200

    For Main programs, update code the codbase wrt this version.

--- AgileWays/src/test/java/com/agileways/demo/DemoMainTest.java
+++ AgileWays/src/test/java/com/agileways/demo/DemoMainTest.java
@@ -3,14 +3,15 @@ package com.agileways.demo;
 public class DemoMainTest {
 
 	public static void main(String[] args) {
-		System.setProperty("java.rmi.server.ignoreStubClasses", "true");
-		String codeBaseAgileFramesCore = " http://localhost:5001/agileframes-core-1.0.2-SNAPSHOT.jar";
-		String codeBaseAgileFramesFoRcEs = " http://localhost:5002/agileframes-forces-1.0.2-SNAPSHOT.jar";
-		String codeBaseAgileFramesTrAcEs = " http://localhost:5003/agileframes-traces-1.0.2-SNAPSHOT.jar";
-		// String codeBaseAgileFramesSeRvEs = " http://localhost:5004/agileframes-services-1.0.2-SNAPSHOT.jar";
-		String codeBaseAgileFramesVR = " http://localhost:5005/agileframes-vr-1.0.2-SNAPSHOT.jar";
-		String codeBaseAgileWays = " http://localhost:6001/agileways-1.0.2-SNAPSHOT.jar";
 		
+		System.setProperty("java.rmi.server.ignoreStubClasses", "true");
+		String version = "1.0.2";
+		String codeBaseAgileFramesCore = " http://localhost:5001/agileframes-core-"+version+".jar";
+		String codeBaseAgileFramesFoRcEs = " http://localhost:5002/agileframes-forces-"+version+".jar";
+		String codeBaseAgileFramesTrAcEs = " http://localhost:5003/agileframes-traces-"+version+".jar";
+		// String codeBaseAgileFramesSeRvEs = " http://localhost:5004/agileframes-services-"+version+".jar";
+		String codeBaseAgileFramesVR = " http://localhost:5005/agileframes-vr-"+version+".jar";
+		String codeBaseAgileWays = " http://localhost:6001/agileways-"+version+".jar";
 		System.setProperty("java.rmi.server.codebase", codeBaseAgileFramesCore + //
 				codeBaseAgileFramesFoRcEs + //
 				codeBaseAgileFramesTrAcEs + //
